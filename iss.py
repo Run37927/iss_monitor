daef4f86e0ef4fef8842d1bba30604d7ba5e0a5a
Hairun Huang <hairunhuang@gmail.com>
2022-08-18 18:46:46 -0700

    tracking iss movement every 5 second

--- iss.py
+++ iss.py
@@ -2,36 +2,50 @@ import requests
 import json
 import turtle
 
+iss = turtle.Turtle()
+
+
+def setup(window):
+    global iss
+
+    window.setup(1000, 500)
+    window.bgpic('earth.png')
+    window.setworldcoordinates(-180, -90, 180, 90)
+    turtle.register_shape("iss.gif")
+    iss.shape("iss.gif")
+
 
 def move_iss(latitude, longitude):
     global iss
 
     iss.penup()
-    iss.goto(long, lat)
+    iss.goto(longitude, latitude)
     iss.pendown()
 
 
-screen = turtle.Screen()
-screen.setup(1000, 500)
-screen.bgpic('earth.png')
-screen.setworldcoordinates(-180, -90, 180, 90)
+def track_iss():
+    url = 'http://api.open-notify.org/iss-now.json'
+    response = requests.get(url)
 
-iss = turtle.Turtle()
-turtle.register_shape("iss.gif")
-iss.shape("iss.gif")
-
-url = 'http://api.open-notify.org/iss-now.json'
-response = requests.get(url)
-
-if response.status_code == 200:
-    response_dict = json.loads(response.text)
-    position = response_dict['iss_position']
-    print('International Space Station at ' + position['latitude'] + ', ' + position['longitude'])
-    lat = float(position['latitude'])
-    long = float(position['longitude'])
-    move_iss(lat, long)
-else:
-    print("Houston, we've got a problem: ", response.status_code)
+    if response.status_code == 200:
+        response_dict = json.loads(response.text)
+        position = response_dict['iss_position']
+        lat = float(position['latitude'])
+        long = float(position['longitude'])
+        move_iss(lat, long)
+    else:
+        print("error: ", response.status_code)
+    widget = turtle.getcanvas()
+    widget.after(5000, track_iss())
 
 
-turtle.mainloop()
+def main():
+    global iss
+    screen = turtle.Screen()
+    setup(screen)
+    track_iss()
+
+
+if __name__ == "__main__":
+    main()
+    turtle.mainloop()

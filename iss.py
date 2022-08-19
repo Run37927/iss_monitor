f9d6bbfb0b1f2ace5d5b7594dbdc78cc03e0d21c
Hairun Huang <hairunhuang@gmail.com>
2022-08-18 18:29:16 -0700

    test flight

--- iss.py
+++ iss.py
@@ -2,6 +2,15 @@ import requests
 import json
 import turtle
 
+
+def move_iss(latitude, longitude):
+    global iss
+
+    iss.penup()
+    iss.goto(long, lat)
+    iss.pendown()
+
+
 screen = turtle.Screen()
 screen.setup(1000, 500)
 screen.bgpic('earth.png')
@@ -18,6 +27,9 @@ if response.status_code == 200:
     response_dict = json.loads(response.text)
     position = response_dict['iss_position']
     print('International Space Station at ' + position['latitude'] + ', ' + position['longitude'])
+    lat = float(position['latitude'])
+    long = float(position['longitude'])
+    move_iss(lat, long)
 else:
     print("Houston, we've got a problem: ", response.status_code)
 

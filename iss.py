2cbe7fb668ec9449c565a6cf30a7adb439580fef
Hairun Huang <hairunhuang@gmail.com>
2022-08-18 18:16:57 -0700

    set up the world map inside turtle screen

--- iss.py
+++ iss.py
@@ -1,5 +1,12 @@
 import requests
 import json
+import turtle
+
+screen = turtle.Screen()
+screen.setup(1000, 500)
+screen.bgpic('earth.png')
+screen.setworldcoordinates(-180, -90, 180, 90)
+
 
 url = 'http://api.open-notify.org/iss-now.json'
 response = requests.get(url)
@@ -10,3 +17,6 @@ if response.status_code == 200:
     print('International Space Station at ' + position['latitude'] + ', ' + position['longitude'])
 else:
     print("Houston, we've got a problem: ", response.status_code)
+
+
+turtle.mainloop()

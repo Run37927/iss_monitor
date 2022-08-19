888f5b8e8cbf3f27bbef765e22b1930da0c90d2f
Hairun Huang <hairunhuang@gmail.com>
2022-08-18 17:55:08 -0700

    testing response message

---
+++ iss.py
@@ -0,0 +1,9 @@
+import requests
+
+url = 'http://api.open-notify.org/iss-now.json'
+response = requests.get(url)
+
+if response.status_code == 200:
+    print(response.text)
+else:
+    print("Houston, we've got a problem: ", response.status_code)

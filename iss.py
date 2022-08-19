32f1e80e4730fde78cfb8c6c0c704ef653221ba6
Hairun Huang <hairunhuang@gmail.com>
2022-08-18 18:04:26 -0700

    convert json iss location to dictionary

--- iss.py
+++ iss.py
@@ -5,6 +5,8 @@ url = 'http://api.open-notify.org/iss-now.json'
 response = requests.get(url)
 
 if response.status_code == 200:
-    print(response.text)
+    response_dict = json.loads(response.text)
+    position = response_dict['iss_position']
+    print('International Space Station at ' + position['latitude'] + ', ' + position['longitude'])
 else:
     print("Houston, we've got a problem: ", response.status_code)
